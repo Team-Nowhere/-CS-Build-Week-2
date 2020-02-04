57d074bcdf058b1f467335593824cdd5c2a51506
Arvin Agas <arvagas@gmail.com>
2020-02-04 17:38:53 -0500

    fixed some bugs when running through the BFS results

--- traversal.py
+++ traversal.py
@@ -28,6 +28,11 @@ if init_res['room_id'] not in mapped_area:
         data[f'{direction}'] = '?'
     mapped_area[init_res['room_id']] = data
 
+    # Save updated mapped_area
+    print('========== Updating map_file...')
+    with open('traversal_graph.json', 'w') as map_file:
+        json.dump(mapped_area, map_file, indent=4)
+
 while len(mapped_area) != 500:
     crd = dict() # current_room_dictonary
 
@@ -73,6 +78,7 @@ while len(mapped_area) != 500:
         # Randomly shuffle since we don't know where we're going anyway
         random.shuffle(unknown_exits)
         direction = unknown_exits[0]
+        print(f'Heading {direction}...')
         move_res = move(direction)
         cooldown(move_res)
         print('>>>>>>>>>> Heading into new room...')
@@ -94,19 +100,29 @@ while len(mapped_area) != 500:
             json.dump(mapped_area, map_file, indent=4)
     # Otherwise, find a way back to closest room with an unknown exit
     else:
-        print(f'========== Starting a BFS...')
+        print('========== Starting a BFS...')
         # Find closest room via BFS
         path_to_next = bfs(current_room_id, mapped_area)
-
+        
+        print(f'Proposed path back: {path_to_next}')
         # Make sure there's actually something being returned
         if path_to_next is not None and len(path_to_next) > 0:
             # Have the player travel back to room with unknown exits
             for index in range(len(path_to_next) - 1):
                 for direction in mapped_area[path_to_next[index]]:
                     if mapped_area[path_to_next[index]][direction] == path_to_next[index + 1]:
+                        print(f'Heading {direction}...')
+                        print(f'Next room should be {path_to_next[index + 1]}...')
                         move_res = move(direction, path_to_next[index + 1])
                         cooldown(move_res)
                         bfs_room_id = move_res['room_id']
-                        print(f'!!!!!!!!!! Currently in room {bfs_room_id}')
+                        print(f'>>>>>>>>>> Made it to room {bfs_room_id}')
+
+                        # Update the current_room variables and prev_room_id
+                        prev_room_id = current_room_id
+                        current_room_id = move_res['room_id']
+                        current_room_title = move_res['title']
+                        current_room_exits = move_res['exits']
+                        current_room_messages = move_res['messages']
         else:
             break
