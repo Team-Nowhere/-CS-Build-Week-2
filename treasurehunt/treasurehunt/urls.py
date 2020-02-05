2dbda9443ed5a45cd0b8c9b6a005beee45fcc965
Adetunji <Ashennaike@gmail.com>
2020-02-05 00:23:49 -0500

    basic templates setup

--- treasurehunt/treasurehunt/urls.py
+++ treasurehunt/treasurehunt/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from treasurehunt.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+  path('admin/', admin.site.urls),
+  path('about/', about),
+  path('game/', game),
+  path('', home),
 ]
---
+++ treasurehunt/treasurehunt/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+  return render(request, 'homepage.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def game(request):
+  return render(request, 'gamepage.html')
